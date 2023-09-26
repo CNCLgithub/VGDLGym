07fa12ea21faf1a3c7dd420df29a15ff083cd5f5
belledon <mbelledonne@gmail.com>
2023-09-26 09:56:56 -0400

    drafting gm

--- src/VGDLGym.jl
+++ src/VGDLGym.jl
@@ -4,9 +4,6 @@ using VGDL
 using Gen
 using Gen_Compose
 
-# Define agents that infer the world
-abstract type GenAgent <: VGDL.Agent end
-
 abstract type ObservationModel end
 
 """
@@ -16,7 +13,14 @@ Produces a `Gen.ChoiceMap` that can be used for inverse-graphics.
 """
 function render end
 
-include("observation_models.jl")
+struct SimpleGraphics <: ObservationModel
+    color_map::Dict #TODO
+end
+
+function render(om::SimpleGraphics, st::GameState)
+    #TODO
+    # something like: https://github.com/YoyoZhang24/VGDL/blob/main/src/scene.jl#L69-L90
+end
 
 struct ChoiceMapObservation <: VGDL.Observation
     cm::Gen.ChoiceMap
@@ -29,6 +33,27 @@ function VGDL.observe(agent::GenAgent, i::Int64, gs::GameState, kdtree::KDTree)
     cm[:kernel => gs.time => :observe] = render(om, gs)
     ChoiceMapObservation(cm)
 end
+
+
+abstract type WorldModel end
+
+abstract type PlanningModule end
+
+struct VGDLWorldModel <: WorldModel
+    game::VGDL.Game
+end
+
+# Define agents that infer the world
+abstract type GenAgent <: VGDL.Agent end
+
+"""
+    observation_model(a::GenAgent)
+
+The observation model associated with the agent
+"""
+function observation_model end
+
+struct
 
 function VGDL.plan(agent::GenAgent, i::Int64, obs::ChoiceMapObservation)
 
